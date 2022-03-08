678c1a638aebc26450ac3126627f49e557e165d7
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2022-03-08 12:23:18 -0400

    Agregar archivos de proyecto.

---
+++ Automatizado/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Automatizado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            //Navegacion de la pagina
+
+            driver.Navigate().GoToUrl("http://localhost/ProyectoSelenium/");
+
+            //Seleccionar la marca del celular
+
+            driver.FindElement(By.Id("celulares")).SendKeys("Xiaomi");
+
+            //Seleccionar la canticada de celulares
+
+            driver.FindElement(By.Id("cantidad")).SendKeys("3");
+
+            //Seleccionar el metodo de pago
+
+            driver.FindElement(By.Id("efectivo")).Click();
+
+            //Seleccionar el boton de pago
+
+
+            System.Threading.Thread.Sleep(5000);
+            driver.FindElement(By.Id("pagar")).Click();
+          
+
+             
+        }
+    }
+}
