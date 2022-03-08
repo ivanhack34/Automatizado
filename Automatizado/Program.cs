2039358a02b8bec971b7cda4ed68e135a593f771
ivanhack34 <48842215+ivanhack34@users.noreply.github.com>
2022-03-08 18:42:02 -0400

    Puse el tiempo de 3 segundo y 5 segundo al boton

--- Automatizado/Program.cs
+++ Automatizado/Program.cs
@@ -19,16 +19,20 @@ namespace Automatizado
             driver.Navigate().GoToUrl("http://localhost/ProyectoSelenium/");
 
             //Seleccionar la marca del celular
-
+            System.Threading.Thread.Sleep(3000);
             driver.FindElement(By.Id("MainContent_celulares")).SendKeys("Xiaomi");
 
             //Seleccionar la canticada de celulares
-
+            System.Threading.Thread.Sleep(3000);
             driver.FindElement(By.Id("MainContent_cantidad")).SendKeys("3");
 
             //Seleccionar el metodo de pago
-
+            System.Threading.Thread.Sleep(3000);
             driver.FindElement(By.Id("MainContent_efectivo")).Click();
+
+            //Seleccionar Condicion del celular
+            System.Threading.Thread.Sleep(3000);
+            driver.FindElement(By.Id("MainContent_nuevo")).Click();
 
             //Seleccionar el boton de pago
 
